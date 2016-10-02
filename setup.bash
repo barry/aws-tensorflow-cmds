fe0f9050120f021a9e3f0dadeeb056543315b265
Barry Devereux <barry@users.noreply.github.com>
2016-10-02 21:06:43 +0100

    run instance with larger root volume

--- setup.bash
+++ setup.bash
@@ -1,5 +1,5 @@
 # Launch instance
-aws ec2 run-instances --image-id ami-fce3c696 --count 1 --instance-type g2.2xlarge --key-name my_aws_key --security-groups my-sg
+aws ec2 run-instances --image-id ami-fce3c696 --count 1 --instance-type g2.2xlarge --key-name my_aws_key --security-groups my-sg --block-device-mapping "[ { \"DeviceName\": \"/dev/sda1\", \"Ebs\": { \"VolumeSize\": 60 } } ]"
 aws_get_ip
 ssh -i ~/.aws/my_aws_key.pem ubuntu@52.91.23.212
 
